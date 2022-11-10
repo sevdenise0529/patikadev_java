73a522bab0ac4181a4d1d0baa73b5bba9bcb1a70
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-10 18:25:15 +0300

    Create dairedilimAlanı.java

---
+++ dairedilimAlanı.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double pi=3.14;
+        System.out.print("Yarıçapı giriniz: ");
+        int r = input.nextInt();
+        System.out.print("Merkez açısını giriniz: ");
+        int a = input.nextInt();
+        
+        double alan = (pi*(r*r)*a)/360;
+        System.out.println("Daire Diliminin Alanı: " + alan);
+    }
+}    
