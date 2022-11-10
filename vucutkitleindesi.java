3ba702956beecc232d0d94fe9af4ab913451d527
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-10 18:44:31 +0300

    Create vucutkitleindesi.java

---
+++ vucutkitleindesi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        double m = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        double kg = input.nextDouble();
+        
+        double indeks = kg/(m*m);
+        System.out.println("Vücut Kitle İndeksiniz: " + indeks);
+    } 
+}
