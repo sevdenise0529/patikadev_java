18afb6662e76fe5e7b829cf4e034bca6591dee17
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-10 17:16:37 +0300

    Create taksimetre.java

---
+++ taksimetre.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double perKm=2.20, starPrice=10;
+        System.out.print("Mesafeyi KM cinsinden giriniz: ");
+        int km = input.nextInt();
+        double total = km*perKm;
+        total += starPrice;
+        total = (total<20) ? 20 : total;
+        System.out.println("Tutarınız: " + total);
+    }
+}    
