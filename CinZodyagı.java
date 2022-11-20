129c348203730e721317ad6c9a10f9c9899d1910
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-20 23:54:23 +0300

    Update CinZodyagı.java

--- CinZodyagı.java
+++ CinZodyagı.java
@@ -1,6 +1,6 @@
 import  java.util.Scanner;
 
-public class Main {
+public class CinZodyagı {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Doğum Yılınızı Giriniz: ");
