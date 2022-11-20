381feed8af3980c420ad32a7117d6b1a157e7e45
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-20 20:21:03 +0300

    Create GirilenSayi.java

---
+++ GirilenSayi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k;
+        int sayi=0, toplam=0,sonuc;
+        System.out.print("Sayiyi giriniz:");
+        k = input.nextInt();
+        for(int i=0; i<=k; i++){
+            if (i%3==0 && i%4==0){
+                System.out.println(i);
+                toplam+=i;
+                sayi++
+            }
+        }
+        sonuc = toplam/(sayi-1);
+        System.out.println("Sonuc: "+sonuc);
+    }
+}
