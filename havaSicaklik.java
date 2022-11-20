1af99f463740e79ddb0521b1d92f8f16b0da3079
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-20 23:54:51 +0300

    Update havaSicaklik.java

--- havaSicaklik.java
+++ havaSicaklik.java
@@ -1,6 +1,6 @@
 import  java.util.Scanner;
 
-public class Main {
+public class havaSicaklik {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Sıcaklık Giriniz: ");
