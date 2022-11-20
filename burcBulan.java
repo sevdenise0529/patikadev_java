bf6a659ef1226c21cbe286373063d73b2d69715a
sevdenise0529 <101528396+sevdenise0529@users.noreply.github.com>
2022-11-20 23:54:17 +0300

    Update burcBulan.java

--- burcBulan.java
+++ burcBulan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class burcBulan {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Doğduğunuz ay: ");
